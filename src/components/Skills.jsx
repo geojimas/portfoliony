9e0c174c1747e92667661ea3b9697c70673110b0
Dimitris Georgiadis <geojimas@gmail.com>
2024-02-09 21:25:27 +0200

    remove commends

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,5 @@
-/* eslint-disable react/jsx-key */
 import React from 'react'
 import { LogosVue } from './techs/Vue'
-// import { LogosLaravel } from './techs/Laravel'
 import { LogosTailwindcssIcon } from './techs/Tailwind'
 import { LogosTypescriptIcon } from './techs/TypeScript'
 import { LogosJavascript } from './techs/Javascript'
@@ -17,7 +15,6 @@ const logos = [
   <LogosReact />,
   <LogosTailwindcssIcon />,
   <LogosNodejsIcon />,
-  // <LogosLaravel />,
   <LogosGitIcon />
 ]
 
