a36c19429efc5f2b9942f8e1a8f0ad2f36b01f5a
Dimitris Georgiadis <geojimas@gmail.com>
2024-12-19 00:06:20 +0200

    resume button update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,19 +1,16 @@
 import { motion } from 'framer-motion'
+import ResumeButton from './ResumeButton.jsx'
 
 export const About = () => {
-  const info = [
-    { text: 'Years experience', count: '3' },
-    { text: 'Completed Projects', count: '8' },
-    { text: 'Companies Work', count: '2' }
-  ]
+  const info = [{ text: 'Years experience', count: '3' }, {
+    text: 'Completed Projects',
+    count: '8'
+  }, { text: 'Companies Work', count: '2' }]
 
-  const aboutMe = [
-    {
-      text: "Hi i'm a Jim, Frontend Web Developer, i have bachelor degree in computer science. I mainly work with VueJS, Vuex, TailwindCSS, NodeJS. If you want check out my projects on GitHub and my Resume."
-    }
-  ]
-  return (
-    <motion.div
+  const aboutMe = [{
+    text: 'Hi i\'m a Jim, Frontend Web Developer, i have bachelor degree in computer science. I mainly work with VueJS, Vuex, TailwindCSS, NodeJS. If you want check out my projects on GitHub and my Resume.'
+  }]
+  return (<motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ ease: 'easeIn', duration: 1 }}>
@@ -25,39 +22,24 @@ export const About = () => {
           <div className="flex md:flex-row flex-col-reverse items-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto">
             <div className="p-2">
               <div className="text-gray-300 my-4">
-                {aboutMe.map(content => (
-                  <p key={content} className="text-justify leading-7 w-11/12 mx-auto">
+                {aboutMe.map(content => (<p key={content} className="text-justify leading-7 w-11/12 mx-auto">
                     {content.text}
-                  </p>
-                ))}
+                  </p>))}
                 <div className="flex mt-10 items-center gap-7">
-                  {info.map(content => (
-                    <div key={content.text}>
+                  {info.map(content => (<div key={content.text}>
                       <h3 className="md:text-4xl text-2xl font-semibold text-yellow-400">
                         {content.count}
                       </h3>
                       <span className="md:text-base text-xs">{content.text}</span>
-                    </div>
-                  ))}
+                    </div>))}
                 </div>
-                <br />
                 <br />
                 <div className="flex justify-start">
                   <a
                     target={'_blank'}
                     href="https://www.canva.com/design/DAEsOtedHcw/jbCPzBBTbc125M5MXdEr4w/edit?utm_content=DAEsOtedHcw&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
-                    className="relative inline-flex items-center px-8 py-3 overflow-hidden text-white border-2 rounded group focus:outline-none hover:text-sky-950 hover:bg-yellow-500 hover:shadow-lg"
                     rel="noreferrer">
-                    <span className="absolute right-0 transition-transform translate-x-full group-hover:-translate-x-4">
-                      <img
-                        src="https://api.iconify.design/emojione-v1:right-arrow.svg"
-                        alt="arrow"
-                      />
-                    </span>
-
-                    <span className="text-md font-bold transition-all group-hover:mr-4">
-                      Resume
-                    </span>
+                    <ResumeButton text={'Resume'} />
                   </a>
                   {/* <a
                     target={'_blank'}
@@ -83,7 +65,8 @@ export const About = () => {
                 <section className="bg-gray-900 text-gray-100 rounded-3xl">
                   <div className="container transition-all max-w-5xl px-4 py-12 mx-auto">
                     <div className="relative mx-8 col-span-12 px-4 space-y-6 sm:col-span-9">
-                      <div className="col-span-12 space-y-12 relative sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:bg-gray-500">
+                      <div
+                        className="col-span-12 space-y-12 relative sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:bg-gray-500">
                         {/* <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:dark:bg-violet-400">
                           <h3 className="text-xl font-semibold tracking-wide">
                             Donec porta enim vel{' '}
@@ -96,7 +79,8 @@ export const About = () => {
                             Interdum et malesuada fames ac ante ipsum primis in faucibus.
                           </p>
                         </div> */}
-                        <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-green-400">
+                        <div
+                          className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-green-400">
                           <h3 className="text-xl font-semibold tracking-wide">
                             arx.net S.A / OPAP S.A
                           </h3>
@@ -105,7 +89,8 @@ export const About = () => {
                           </time>
                           <p className="mt-3">Frontend Web Developer</p>
                         </div>
-                        <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-red-400">
+                        <div
+                          className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-red-400">
                           <h3 className="text-xl font-semibold tracking-wide">Lancom Ltd</h3>
                           <time className="text-xs tracking-wide uppercase text-gray-400">
                             Mar 2021 - Sep 2021
@@ -121,6 +106,5 @@ export const About = () => {
           </div>
         </div>
       </section>
-    </motion.div>
-  )
+    </motion.div>)
 }
