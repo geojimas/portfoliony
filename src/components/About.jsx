823aaf0e9455389ca45194033b17f7fe2e28e578
Dimitris Georgiadis <geojimas@gmail.com>
2025-03-23 23:41:26 +0200

    refactor card and button ui

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,110 +1,98 @@
 import { motion } from 'framer-motion'
-import ResumeButton from './ResumeButton.jsx'
+import { ResumeButton } from './ResumeButton.jsx'
+import { Card } from './Card.jsx'
 
 export const About = () => {
   const info = [{ text: 'Years experience', count: '3' }, {
-    text: 'Completed Projects',
-    count: '8'
+    text: 'Completed Projects', count: '8'
   }, { text: 'Companies Work', count: '2' }]
 
   const aboutMe = [{
     text: 'Hi i\'m a Jim, Frontend Web Developer, i have bachelor degree in computer science. I mainly work with VueJS, Vuex, TailwindCSS, NodeJS. If you want check out my projects on GitHub and my Resume.'
   }]
   return (<motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ ease: 'easeIn', duration: 1 }}>
-      <section id="about" className="py-16 text-white">
-        <div className="text-center mt-8">
-          <h3 className="text-4xl font-semibold">
-            About <span className="text-yellow-400">Me</span>
-          </h3>
-          <div className="flex md:flex-row flex-col-reverse items-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto">
-            <div className="p-2">
-              <div className="text-gray-300 my-4">
-                {aboutMe.map(content => (<p key={content} className="text-justify leading-7 w-11/12 mx-auto">
-                    {content.text}
-                  </p>))}
-                <div className="flex mt-10 items-center gap-7">
-                  {info.map(content => (<div key={content.text}>
-                      <h3 className="md:text-4xl text-2xl font-semibold text-yellow-400">
-                        {content.count}
-                      </h3>
-                      <span className="md:text-base text-xs">{content.text}</span>
-                    </div>))}
-                </div>
-                <br />
-                <div className="flex justify-start">
-                  <a
-                    target={'_blank'}
-                    href="https://www.canva.com/design/DAEsOtedHcw/jbCPzBBTbc125M5MXdEr4w/edit?utm_content=DAEsOtedHcw&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
-                    rel="noreferrer">
-                    <ResumeButton text={'Resume'} />
-                  </a>
-                  {/* <a
-                    target={'_blank'}
-                    href="https://drive.google.com/file/d/13N0k6XVarBfqA3k8o_JjjbdBzSZuB7Hs/view?usp=share_link"
-                    className="relative inline-flex items-center px-8 py-3 overflow-hidden text-white border-2 rounded group focus:outline-none ml-10 hover:text-sky-950 hover:bg-yellow-500 hover:shadow-lg"
-                    rel="noreferrer">
-                    <span className="absolute right-0 transition-transform translate-x-full group-hover:-translate-x-4">
-                      <img
-                        src="https://api.iconify.design/emojione-v1:right-arrow.svg"
-                        alt="arrow"
-                      />
-                    </span>
-
-                    <span className="text-md font-bold transition-all group-hover:mr-4">
-                      Thesis
-                    </span>
-                  </a> */}
-                </div>
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    transition={{ ease: 'easeIn', duration: 1 }}>
+    <section id="about" className="py-16 text-white">
+      <div className="text-center mt-8">
+        <h3 className="text-4xl font-semibold">
+          About <span className="text-yellow-400">Me</span>
+        </h3>
+        <div className="flex md:flex-row flex-col-reverse items-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto">
+          <div className="p-2">
+            <div className="text-gray-300 my-4">
+              {aboutMe.map(content => (<p key={content} className="text-justify leading-7 w-11/12 mx-auto">
+                {content.text}
+              </p>))}
+              <div className="flex justify-center mt-10 items-center gap-7">
+                {info.map(content => (<div key={content.text}>
+                  <h3 className="md:text-4xl text-2xl font-semibold text-yellow-400">
+                    {content.count}
+                  </h3>
+                  <span className="md:text-base text-xs">{content.text}</span>
+                </div>))}
               </div>
-            </div>
-            <div className="flex-1 md:mt-0 mt-6 flex justify-center items-center hover:scale-105">
-              <div className="lg:w-96 h-full relative sm:w-10/12 w-11/12 max-w-sm aboutImg ">
-                <section className="bg-gray-900 text-gray-100 rounded-3xl">
-                  <div className="container transition-all max-w-5xl px-4 py-12 mx-auto">
-                    <div className="relative mx-8 col-span-12 px-4 space-y-6 sm:col-span-9">
-                      <div
-                        className="col-span-12 space-y-12 relative sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:bg-gray-500">
-                        {/* <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:dark:bg-violet-400">
-                          <h3 className="text-xl font-semibold tracking-wide">
-                            Donec porta enim vel{' '}
-                          </h3>
-                          <time className="text-xs tracking-wide uppercase dark:text-gray-400">
-                            Dec 2020
-                          </time>
-                          <p className="mt-3">
-                            Pellentesque feugiat ante at nisl efficitur, in mollis orci scelerisque.
-                            Interdum et malesuada fames ac ante ipsum primis in faucibus.
-                          </p>
-                        </div> */}
-                        <div
-                          className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-green-400">
-                          <h3 className="text-xl font-semibold tracking-wide">
-                            arx.net S.A / OPAP S.A
-                          </h3>
-                          <time className="text-xs tracking-wide uppercase text-gray-400">
-                            Nov 2021 - Present
-                          </time>
-                          <p className="mt-3">Frontend Web Developer</p>
-                        </div>
-                        <div
-                          className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-red-400">
-                          <h3 className="text-xl font-semibold tracking-wide">Lancom Ltd</h3>
-                          <time className="text-xs tracking-wide uppercase text-gray-400">
-                            Mar 2021 - Sep 2021
-                          </time>
-                          <p className="mt-3">Junior Full Stack Web Developer</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </section>
+              <br />
+              <div className="flex justify-center">
+                <a
+                  target={'_blank'}
+                  href="https://www.canva.com/design/DAEsOtedHcw/jbCPzBBTbc125M5MXdEr4w/edit?utm_content=DAEsOtedHcw&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
+                  rel="noreferrer">
+                  <ResumeButton text={'Resume'} />
+                </a>
+                {/* <a */}
+                {/*  target={'_blank'} */}
+                {/*  href="https://drive.google.com/file/d/13N0k6XVarBfqA3k8o_JjjbdBzSZuB7Hs/view?usp=share_link" */}
+                {/*  className="relative inline-flex items-center px-8 py-3 overflow-hidden text-white border-2 rounded group focus:outline-none ml-10 hover:text-sky-950 hover:bg-yellow-500 hover:shadow-lg" */}
+                {/*  rel="noreferrer"> */}
+                {/*    <span className="absolute right-0 transition-transform translate-x-full group-hover:-translate-x-4"> */}
+                {/*      <img */}
+                {/*        src="https://api.iconify.design/emojione-v1:right-arrow.svg" */}
+                {/*        alt="arrow" */}
+                {/*      /> */}
+                {/*    </span> */}
+
+                {/*  <span className="text-md font-bold transition-all group-hover:mr-4"> */}
+                {/*      Thesis */}
+                {/*    </span> */}
+                {/* </a> */}
               </div>
             </div>
           </div>
+          <div className="flex-1 md:mt-0 mt-6 flex justify-center items-center hover:scale-105">
+            <div className="lg:w-96 h-full relative sm:w-10/12 w-11/12 max-w-sm">
+              <Card>
+                <div className="container transition-all max-w-5xl px-4 py-12 mx-auto">
+                  <div className="relative mx-8 col-span-12 px-4 space-y-6 sm:col-span-9">
+                    <div
+                      className="col-span-12 space-y-12 relative sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:bg-gray-500">
+                      <div
+                        className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-green-400">
+                        <h3 className="text-xl font-semibold tracking-wide">
+                          ΑΡΧ ΝΕΤ Α.Ε.
+                        </h3>
+                        <time className="text-xs tracking-wide uppercase text-gray-400">
+                          Nov 2021 - Present
+                        </time>
+                        <p className="mt-3">Frontend Web Developer</p>
+                      </div>
+                      <div
+                        className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-red-400">
+                        <h3 className="text-xl font-semibold tracking-wide">Lancom Ltd</h3>
+                        <time className="text-xs tracking-wide uppercase text-gray-400">
+                          Mar 2021 - Aug 2021
+                        </time>
+                        <p className="mt-3">Junior Full Stack Web Developer</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Card>
+            </div>
+          </div>
         </div>
-      </section>
-    </motion.div>)
+      </div>
+    </section>
+  </motion.div>)
 }
--- src/components/ResumeButton.jsx
+++ src/components/ResumeButton.jsx
@@ -1,22 +1,103 @@
 import React from 'react'
-import './ResumeButton.css'
+import styled from 'styled-components'
 
-const ResumeButton = ({ text }) => {
-  return (
-    <button className="animated-button">
-      <svg xmlns="http://www.w3.org/2000/svg" className="arr-2" viewBox="0 0 24 24">
-        <path
-          d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
-        ></path>
-      </svg>
-      <span className="text">{text}</span>
-      <span className="circle"></span>
-      <svg xmlns="http://www.w3.org/2000/svg" className="arr-1" viewBox="0 0 24 24">
-        <path
-          d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
-        ></path>
-      </svg>
+export const ResumeButton = (props) => {
+  return (<StyledWrapper>
+    <button className="learn-more">
+        <span className="circle" aria-hidden="true">
+          <span className="icon arrow" />
+        </span>
+      <span className="button-text">{props.text}</span>
     </button>
-  )
+  </StyledWrapper>)
 }
-export default ResumeButton
+
+const StyledWrapper = styled.div`
+    button {
+        position: relative;
+        display: inline-block;
+        cursor: pointer;
+        outline: none;
+        border: 0;
+        vertical-align: middle;
+        text-decoration: none;
+        background: transparent;
+        padding: 0;
+        font-size: inherit;
+        font-family: inherit;
+    }
+
+    button.learn-more {
+        width: 12rem;
+        height: auto;
+    }
+
+    button.learn-more .circle {
+        transition: all 0.45s cubic-bezier(0.65, 0, 0.076, 1);
+        position: relative;
+        display: block;
+        margin: 0;
+        width: 3rem;
+        height: 3rem;
+        background: #FACD15;
+        border-radius: 1.625rem;
+    }
+
+    button.learn-more .circle .icon {
+        transition: all 0.45s cubic-bezier(0.65, 0, 0.076, 1);
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        margin: auto;
+        background: #111827;
+    }
+
+    button.learn-more .circle .icon.arrow {
+        transition: all 0.45s cubic-bezier(0.65, 0, 0.076, 1);
+        left: 0.625rem;
+        width: 1.125rem;
+        height: 0.125rem;
+        background: none;
+    }
+
+    button.learn-more .circle .icon.arrow::before {
+        position: absolute;
+        content: "";
+        top: -0.29rem;
+        right: 0.0625rem;
+        width: 0.625rem;
+        height: 0.625rem;
+        border-top: 0.125rem solid #fff;
+        border-right: 0.125rem solid #fff;
+        transform: rotate(45deg);
+    }
+
+    button.learn-more .button-text {
+        transition: all 0.45s cubic-bezier(0.65, 0, 0.076, 1);
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        padding: 0.75rem 0;
+        margin: 0 0 0 1.85rem;
+        color: #eff0e6;
+        font-weight: 700;
+        line-height: 1.6;
+        letter-spacing: 2px;
+        text-align: center;
+        text-transform: uppercase;
+    }
+
+    button:hover .circle {
+        width: 90%;
+    }
+
+    button:hover .circle .icon.arrow {
+        background: #fff;
+        transform: translate(1rem, 0);
+    }
+
+    button:hover .button-text {
+        color: #111827;
+    }`
