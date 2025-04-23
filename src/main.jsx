635f059e147a897355a74de4eada040d2499f3a6
Dimitris Georgiadis <geojimas@gmail.com>
2025-04-24 02:53:49 +0300

    setup gtag

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import ReactGA from 'react-ga4'
 import App from './App'
 import './index.css'
 
+ReactGA.initialize('G-VKP5Z26H1K')
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 )
