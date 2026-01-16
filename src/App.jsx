827c3f0285407a06342e51505dfaf18ff896a821
dimitris georgiadis <geojimas@gmail.com>
2026-01-17 01:34:48 +0200

    Reduces animation fade-in duration for faster load

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      transition={{ ease: 'easeIn', duration: 2 }}
+      transition={{ ease: 'easeIn', duration: 1 }}
     >
       <Navbar />
       <Hero />
