6e47909d2eb4e2348cf9bcd80fe2aac5583c1b87
dimitris georgiadis <geojimas@gmail.com>
2026-01-17 01:28:14 +0200

    Update dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { analyzer } from 'vite-bundle-analyzer'
 import { imagetools } from 'vite-imagetools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), imagetools()],
+  plugins: [react(), tailwindcss(), imagetools(), analyzer()],
 })
