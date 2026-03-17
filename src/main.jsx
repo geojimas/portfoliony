af2325334939304536a471ae09893782119c51c4
dimitris georgiadis <geojimas@gmail.com>
2026-03-17 20:35:27 +0200

    remove ga pkg

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom/client'
-import ReactGA from 'react-ga4'
 import App from './App'
 import './index.css'
-
-ReactGA.initialize('G-VKP5Z26H1K')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
